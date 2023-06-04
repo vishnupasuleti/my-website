8e355f4ca93cb33d53865e3ad58bf05db523e41b
Vishnu Pasuleti <vpasuleti@compass.education>
2023-06-04 14:04:04 +0530

    mobile menu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,14 +2,20 @@
 
 import { appRouteConfig } from "@/config/app-route-config";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import { useRouter } from "next/router";
-import Image from 'next/image';
+import Image from "next/image";
+import { MobileNav } from "./MobileNav";
 
 export const NavBar = () => {
   const pathName = usePathname();
   const [showMobileMenu, setShowMobileMenu] = useState(false);
+
+  useEffect(() => {
+    setShowMobileMenu(false);
+  }, [pathName]);
+
   return (
     <header className="flex items-center justify-center h-20 py-6">
       <div className="flex flex-1 gap-6 md:gap-10">
@@ -22,7 +28,9 @@ export const NavBar = () => {
           <nav className="hidden gap-6 md:flex">
             {appRouteConfig.map((route, index) => (
               <Link
-                className={`flex text-lg font-medium text-center ${ route.href === pathName ? 'text-gray-800' : 'text-gray-500' } transition-colors hover:text-gray-800`}
+                className={`flex text-lg font-medium text-center ${
+                  route.href === pathName ? "text-gray-800" : "text-gray-500"
+                } transition-colors hover:text-gray-800`}
                 href={route.href}
                 key={index}
               >
@@ -31,14 +39,53 @@ export const NavBar = () => {
             ))}
           </nav>
         }
-        <button
-        className="flex items-center space-x-2 md:hidden"
-        onClick={() => setShowMobileMenu(!showMobileMenu)}
-      >
-        <span className="inline-block text-xl font-extrabold text-gray-800">
-            { showMobileMenu ? 'X' :  'V' }
+        <div className="flex items-center justify-between flex-1 space-x-2 md:hidden">
+          <span className="inline-block text-xl font-extrabold text-gray-800">
+            V
           </span>
-      </button>
+          <button
+            className="flex items-center space-x-2 text-gray-800 md:hidden"
+            onClick={() => setShowMobileMenu(!showMobileMenu)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height={25}
+              width={25}
+              viewBox="0 0 24 24"
+              id="menu"
+            >
+              <g data-name="Layer 2">
+                <g data-name="menu">
+                  <rect
+                    width="18"
+                    height="2"
+                    x="3"
+                    y="11"
+                    rx=".95"
+                    ry=".95"
+                  ></rect>
+                  <rect
+                    width="18"
+                    height="2"
+                    x="3"
+                    y="16"
+                    rx=".95"
+                    ry=".95"
+                  ></rect>
+                  <rect
+                    width="18"
+                    height="2"
+                    x="3"
+                    y="6"
+                    rx=".95"
+                    ry=".95"
+                  ></rect>
+                </g>
+              </g>
+            </svg>
+          </button>
+          {showMobileMenu ? <MobileNav /> : null}
+        </div>
       </div>
     </header>
   );
